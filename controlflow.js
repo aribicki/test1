a2d83a8836f34a0ddda30ff81c81dd30c2fb8629
Andrew <aribicki@gmail.com>
2015-02-18 12:57:52 -0500

    pushing up to github

---
+++ controlflow.js
@@ -0,0 +1,9 @@
+// Andrew Ribicki
+// CMP237 test1
+// Write a program that shows all three basic types of control flow. 
+
+ var a = 5  // set a = 5
+    console.log(a) // return a value
+ var b = 5 // set be =5
+    console.log(b) //return b value
+ console.log(a + b) // return value a + value b
