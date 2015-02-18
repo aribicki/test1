1a55c98167d3348c1d855ef4eb0a4bf1276934a0
Andrew <aribicki@gmail.com>
2015-02-18 12:46:41 -0500

    pushing to github

---
+++ variable.js
@@ -0,0 +1,14 @@
+// Andrew Ribicki
+// CMP237 test1
+// Write a program that declares a variable, assigns it a value, and then someho//w changes it by using one of the basic control structuers. Be sure to prin the// result
+ var a = 2; //set a=2 
+ var b = 3; //set b=3
+ var c = 5; //set c=5
+  function min(a,b) {    //call on function min to compare var a and b
+      if (a < b)        // check if a is less than b
+    return a + var c;   // if true return a and add var c
+ else                   // if false
+    return b + var c;   // return b + var c 
+
+ console.log min(a,b) 
+
