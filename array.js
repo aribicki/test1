8f3fc5a6a7a48b0575c137cacdf6c1f26440bb9b
Andrew <aribicki@gmail.com>
2015-02-18 13:48:54 -0500

    pushing up to github

---
+++ array.js
@@ -0,0 +1,11 @@
+// Andrew Ribicki
+// CMP237 test1
+// write a program that declares an array of objects, and then does soemthing to extract and print information of any sort from an array
+ 
+ var list = [
+	{"entry": ["car" , "red" , "small"]
+	{"entry": ["truck" , "blue" , "big"]
+        {"entry": ["car" , "yellow" , "small"]
+];
+ var n = list.length;
+  console.log(n)
